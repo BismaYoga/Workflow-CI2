18c37eef55be124a162db75d77477b65e4cf1e90
Bisma Yoga <bismayoga432@gmail.com>
2025-12-24 09:36:04 +0800

    first commit

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,22 +1,31 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 
-mlflow.sklearn.autolog()
-
-def train_basic():
-    df = pd.read_csv('iris_preprocessing.csv')
-    X = df.drop('target', axis=1)
-    y = df['target']
+def train():
+    df = pd.read_csv('padi_preprocessing.csv') 
+    
+    X = df.drop(columns=['Produksi'])
+    y = df['Produksi']
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    with mlflow.start_run(run_name="Basic_Model_Autolog"):
-        model = RandomForestClassifier(n_estimators=100)
+    
+    with mlflow.start_run():
+        model = RandomForestRegressor(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
-        print("Model Basic selesai dilatih. Cek MLflow UI lokal Anda.")
+        
+        predictions = model.predict(X_test)
+        mae = mean_absolute_error(y_test, predictions)
+        r2 = r2_score(y_test, predictions)
+        
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2_score", r2)
+        mlflow.sklearn.log_model(model, "model")
+        
+        print(f"Retraining complete. MAE: {mae}, R2: {r2}")
 
 if __name__ == "__main__":
-    train_basic()
+    train()
