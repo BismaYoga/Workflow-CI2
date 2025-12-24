6e8f9bd326413fa6e2a5729c276eb29bce3a20c3
Bisma Yoga <bismayoga432@gmail.com>
2025-12-24 09:53:53 +0800

    first commit5

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,8 +7,10 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 def train():
-    data_file = 'padi_preprocessing.csv'
+    # Set lokasi database lokal agar bersih
+    mlflow.set_tracking_uri("file:./mlruns")
     
+    data_file = 'padi_preprocessing.csv'
     if not os.path.exists(data_file):
         print(f"Error: {data_file} tidak ditemukan!")
         return
@@ -19,11 +21,12 @@ def train():
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
+    # Memaksa mengakhiri run yang menggantung dari environment GitHub
     if mlflow.active_run():
         mlflow.end_run()
 
-
-    with mlflow.start_run(run_name="Padi_Retraining_Final"):
+    # Memulai run baru
+    with mlflow.start_run(run_name="Padi_Retraining_Final_Fix"):
         model = RandomForestRegressor(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
         
